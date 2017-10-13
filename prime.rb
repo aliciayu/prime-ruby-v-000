2f9584d74a377340c8468daba22793dfaef6fb25
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-13 01:05:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(num)
-  if Math.sqrt(num).floor.downto(2).each {|i| return false if num % i == 0}
-   true
- else
-   false
+   if Math.sqrt(num).floor.downto(2).each {|i|}
+     true
+   else 
+     return false if num % i == 0
 end
