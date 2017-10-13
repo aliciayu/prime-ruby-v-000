f19e67d431d606118e5943d63ad36eef24b58138
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-13 01:25:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,18 @@
 # Add  code here!
 def prime?(num)
-  2.upto(Math.sqrt(self).to_i) do |x|
-    return false if self%x == 0
+  (2...num).each do |divisor|
+    return false if num % divisor == 0
   end
+
   true
+end
+
+def primes(max)
+  primes = []
+
+  (2...max).each do |num|
+    primes << num if is_prime?(num)
+  end
+
+  primes
 end
