574d14126ccd6e78b9619d5f98322e1a1af3aa32
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-13 01:23:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,7 @@
 # Add  code here!
 def prime?(num)
+  value = -value if value < 0
+    return false if value < 2
   return false if self <= 1
   2.upto(Math.sqrt(self).to_i) do |x|
     return false if self%x == 0
